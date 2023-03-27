84ebbccf4e39212808efa1d1193e34fa8cea09b4
Kaurx14 <99817804+Kaurx14@users.noreply.github.com>
2023-03-27 16:49:19 +0300

    First commit

---
+++ LibraryAppWithGUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryAppWithGUI;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
+
---
+++ LibraryAppWithGUI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LibraryAppWithGUI;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
