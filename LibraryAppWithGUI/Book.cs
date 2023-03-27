d45ba8d6b2ff0724f89711041e414497bfa804ce
Kaurx14 <99817804+Kaurx14@users.noreply.github.com>
2023-03-27 17:37:59 +0300

    second commit

---
+++ LibraryAppWithGUI/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace LibraryAppWithGUI
+{
+    public class Book
+    {
+        public string title;
+        public string author;
+        public string isbn;
+
+        public Book(string title, string author, string isbn)
+        {
+            this.title = title;
+            this.author = author;
+            this.isbn = isbn;
+        }
+    }
+}
---
+++ LibraryAppWithGUI/BorrowedBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace LibraryAppWithGUI
+{
+    public class BorrowedBook
+    {
+        public Book book;
+        public DateTime borrowDate;
+        public User user;
+
+        public BorrowedBook(Book book, DateTime borrowDate, User user)
+        {
+            this.book = book;
+            this.borrowDate = borrowDate;
+            this.user = user;
+        }
+    }
+}
+
